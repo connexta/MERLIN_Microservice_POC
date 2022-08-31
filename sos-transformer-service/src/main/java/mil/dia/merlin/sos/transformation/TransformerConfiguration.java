bf49905c9909009c06f96d413f87d3f012a5f215
Don Cruver <donald.cruver@connexta.com>
2022-08-31 07:57:53 -0700

    added transformer for ui-json

--- sos-transformer-service/src/main/java/mil/dia/merlin/sos/transformation/TransformerConfiguration.java
+++ sos-transformer-service/src/main/java/mil/dia/merlin/sos/transformation/TransformerConfiguration.java
@@ -1,6 +1,13 @@
 package mil.dia.merlin.sos.transformation;
 
+import com.arakelian.jq.ImmutableJqLibrary;
+import com.arakelian.jq.ImmutableJqRequest;
+import com.arakelian.jq.JqLibrary;
+import com.arakelian.jq.JqRequest;
+import com.arakelian.jq.JqResponse;
 import mil.dia.swe.SWETransform;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,6 +15,17 @@ import java.util.function.Function;
 
 @Configuration
 class TransformerConfiguration {
+    private static final String TRANSFORM_FILTER = ".procedureDescription | (.identification.identifier| map({(.name)" +
+            ": (.value)}) | add) + (.identification.identifier| map(if .name == \"uniqueID\" then " +
+            "{\"assignedProcedure\": (.value), \"assignedOffering\": (.value)} else null end) | add) + (if " +
+            ".classification.classifier? then .classification.classifier | map(if .name? then {(.name): (.value)} " +
+            "else null end) | add else null end) + (.validTime | if .type == \"TimeInstant\" then " +
+            "{\"validTimeStart\": (.timePosition.value)} else {\"validTimeStart\": (.beginPosition), " +
+            "\"validTimeEnd\": (.endPosition)} end) + (if .securityConstraint? then {\"securityConstraint\": " +
+            ".securityConstraint} else null end) + (if .legalConstraint? then {\"legalConstraint\": .legalConstraint}" +
+            " else null end) + (if .member.location? then {\"sensorLocation\": (.member.location)} else null end)";
+
+    private final Logger logger = LoggerFactory.getLogger(TransformerConfiguration.class);
 
     @Bean
     public Function<String, String> sensorMessageTransformer() {
@@ -21,7 +39,25 @@ class TransformerConfiguration {
 
     @Bean
     public Function<String, String> sensorUIMessageTransformer() {
-        // Add UI transformation function here
-        return s -> s;
+        return this::uiTransform;
+    }
+
+    private String uiTransform(String input) {
+        JqLibrary library = ImmutableJqLibrary.of();
+
+        final JqRequest request =
+                ImmutableJqRequest.builder().lib(library).input(input).filter(TRANSFORM_FILTER).build();
+
+        final JqResponse response = request.execute();
+
+        if (response.hasErrors()) {
+            for (String error : response.getErrors()) {
+                logger.info(error);
+            }
+
+            return "";
+        }
+
+        return response.getOutput();
     }
 }
