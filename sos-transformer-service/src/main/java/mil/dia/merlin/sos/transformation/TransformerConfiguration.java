b8754a0324cd0c9b4ffef3e43f64e5bf6339c376
Don Cruver <donald.cruver@connexta.com>
2022-08-31 09:00:49 -0700

    simplified UI transformation calls

--- sos-transformer-service/src/main/java/mil/dia/merlin/sos/transformation/TransformerConfiguration.java
+++ sos-transformer-service/src/main/java/mil/dia/merlin/sos/transformation/TransformerConfiguration.java
@@ -1,11 +1,8 @@
 package mil.dia.merlin.sos.transformation;
 
-import com.arakelian.jq.ImmutableJqLibrary;
-import com.arakelian.jq.ImmutableJqRequest;
-import com.arakelian.jq.JqLibrary;
-import com.arakelian.jq.JqRequest;
-import com.arakelian.jq.JqResponse;
+import mil.dia.swe.JSONFilter;
 import mil.dia.swe.SWETransform;
+import net.thisptr.jackson.jq.exception.JsonQueryException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
@@ -38,26 +35,8 @@ class TransformerConfiguration {
     }
 
     @Bean
-    public Function<String, String> sensorUIMessageTransformer() {
-        return this::uiTransform;
-    }
-
-    private String uiTransform(String input) {
-        JqLibrary library = ImmutableJqLibrary.of();
-
-        final JqRequest request =
-                ImmutableJqRequest.builder().lib(library).input(input).filter(TRANSFORM_FILTER).build();
-
-        final JqResponse response = request.execute();
-
-        if (response.hasErrors()) {
-            for (String error : response.getErrors()) {
-                logger.info(error);
-            }
-
-            return "";
-        }
-
-        return response.getOutput();
+    public Function<String, String> sensorUIMessageTransformer() throws JsonQueryException {
+        JSONFilter jsonFilter = new JSONFilter(TRANSFORM_FILTER);
+        return jsonFilter::filter;
     }
 }
